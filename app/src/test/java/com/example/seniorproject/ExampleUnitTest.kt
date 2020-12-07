724429d7e59cd72d9e16bada6ab0ee204933dfc8
parrsi01 <parrsi01@luther.edu>
2020-12-07 17:10:16 -0500

    User Profile Activity created

--- app/src/test/java/com/example/seniorproject/ExampleUnitTest.kt
+++ app/src/test/java/com/example/seniorproject/ExampleUnitTest.kt
@@ -1,10 +1,10 @@
 package com.example.seniorproject
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
+ *
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
